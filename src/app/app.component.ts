5c1781a851a376f78d7527ce7474beba754e8055
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-01-07 16:58:09 +0000

    create new component; templates, interpolation and directives; data binding and pipes

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
 
+// metadata
+// this Angular built-in decorator is used to identify the class below as a component
 @Component({
-  selector: 'pm-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "pm-root", // custom html tag (pm = product management)
+  templateUrl: "./app.component.html", // linked template, instead of inline
+  styleUrls: ["./app.component.css"]
 })
+
+// class
 export class AppComponent {
-  title = 'Angular: Getting Started';
+  // properties (data)
+  pageTitle: string = "Acme Product Management";
+
+  // methods (logic)
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,13 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { FormsModule } from "@angular/forms";
 
-import { AppComponent } from './app.component';
+import { AppComponent } from "./app.component";
+import { ProductListComponent } from "./products/product-list.component";
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  imports: [BrowserModule, FormsModule],
+  declarations: [AppComponent, ProductListComponent],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
