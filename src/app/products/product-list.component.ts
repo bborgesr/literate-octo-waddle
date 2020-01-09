e571e34076cdce3a56e95a8b6a32b5f38b60866c
Barbara Borges Ribeiro <barb.b.ribeiro@gmail.com>
2020-01-09 11:50:46 +0000

    services and dependency injection

--- src/app/products/product-list.component.ts
+++ src/app/products/product-list.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from "@angular/core";
 
 import { IProduct } from "./product";
+import { ProductService } from "./product.service";
 
 @Component({
   selector: "pm-products",
@@ -13,7 +14,7 @@ export class ProductListComponent implements OnInit {
   imageMargin: number = 2;
   showImage: boolean = false;
 
-  _listFilter: string = "cart";
+  _listFilter: string;
   get listFilter(): string {
     return this._listFilter;
   }
@@ -25,42 +26,10 @@ export class ProductListComponent implements OnInit {
   }
 
   filteredProducts: IProduct[];
-  products: IProduct[] = [
-    {
-      productId: 1,
-      productName: "Leaf Rake",
-      productCode: "GDN-0011",
-      releaseDate: "March 19, 2019",
-      description: "Leaf rake with 48-inch wooden handle.",
-      price: 19.95,
-      starRating: 3.2,
-      imageUrl: "assets/images/leaf_rake.png"
-    },
-    {
-      productId: 2,
-      productName: "Garden Cart",
-      productCode: "GDN-0023",
-      releaseDate: "March 18, 2019",
-      description: "15 gallon capacity rolling garden cart",
-      price: 32.99,
-      starRating: 4.2,
-      imageUrl: "assets/images/garden_cart.png"
-    },
-    {
-      productId: 5,
-      productName: "Hammer",
-      productCode: "TBX-0048",
-      releaseDate: "May 21, 2019",
-      description: "Curved claw steel hammer",
-      price: 8.9,
-      starRating: 4.8,
-      imageUrl: "assets/images/hammer.png"
-    }
-  ];
+  products: IProduct[] = [];
 
-  constructor() {
-    this.filteredProducts = this.products;
-    this.listFilter = "cart";
+  constructor(private productService: ProductService) {
+    // this.listFilter = "cart";
   }
 
   performFilter(filterBy: string): IProduct[] {
@@ -81,5 +50,7 @@ export class ProductListComponent implements OnInit {
 
   ngOnInit(): void {
     console.log("In onInit");
+    this.products = this.productService.getProducts();
+    this.filteredProducts = this.products;
   }
 }
